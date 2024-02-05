f2a006f81c15b69556e89c7a404617a8282e92bd
DESKTOP-C3OL7C2\lacun <jorgeVC>
2024-02-05 16:03:29 -0500

    first commit

---
+++ 0001_hello_typescript.ts
@@ -0,0 +1,30 @@
+console.log('Hello Typescript');
+
+//Test Commentary
+
+var version = 'es6'
+console.log(`hello ${version} TypeScript`)
+
+
+var test: string = "a string"
+console.log(`test= ${test}`)
+
+
+//Basic Types
+var myBoolean: boolean = true
+
+var myNumber: number = 1234
+
+var myStringArray: string[] = ['first', 'second', 'third']
+
+console.log(`myBoolean= ${myBoolean}`)
+console.log(`myStringArray= ${myStringArray}`)
+console.log(`myNumber= ${myNumber}`)
+
+
+
+
+
+
+
+
---
+++ 0002_basic_types.ts
@@ -0,0 +1,26 @@
+//Basic Types
+var myBoolean: boolean = true
+
+var myNumber: number = 1234
+
+var myStringArray: string[] = ['first', 'second', 'third']
+
+console.log(`myBoolean= ${myBoolean}`)
+console.log(`myStringArray= ${myStringArray}`)
+console.log(`myNumber= ${myNumber}`)
+
+//Inferred typing
+/*
+TypeScript also uses a technique called inferred typing, or type inference to ndetermine the type of a variable
+*/
+
+var inferredString = "this is a string"
+
+var inferredNumber = 1
+
+inferredNumber = inferredString
+
+
+
+
+
